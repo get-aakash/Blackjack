75ac1d1f36de1f5b7c0408ead13faaf42ac839ec
Aakash Acharya <mail.aakash108@gmail.com>
2024-05-18 12:29:06 +1000

    Dom Html

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
 
-
-function myFunction1(value){
-    document.getElementById("demo").innerHTML=  value
+function myFunction(){
+    document.getElementById("myImage").src = "images/bulbOn.jpeg"
 }
-
-   let myPromise = new Promise(function(resolve, reject){
-    const success = false
-
-    if(success){
-        resolve("Promise success")
-    }
-    else{
-        reject("Promise failed")
-    }
-   })
-
-
-
-   myPromise.then(
-    function(value){
-        myFunction1(value)
-    },
-    function(error){
-        myFunction1(error)
-    }
-    
-)
-
-setTimeout(function(){
-    myFunction1("Hello")
-},3000)
