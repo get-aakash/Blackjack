dda6a216fd1b7140c3c4d71b3ab8984005d452af
Aakash Acharya <mail.aakash108@gmail.com>
2024-05-19 14:49:14 +1000

    w3 school

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function myFunction() {
-    const para = document.createElement("p")
-    const node = document.createTextNode("This is a new node")
-    para.appendChild(node)
-
-    const element = document.getElementById("main")
-    element.appendChild(para)
-
-}
-
 
