c59fdc1703062ef20dc37ef1197d4513b72f774b
Aakash Acharya <mail.aakash108@gmail.com>
2024-07-08 10:49:09 +1000

    crud ui

--- myScript.js
+++ myScript.js
@@ -1,39 +1,19 @@
-const inputBox = document.getElementById("input-box")
-const listContainer = document.getElementById("list-container")
-
+const firstName = document.getElementById("firstName")
+const lastName = document.getElementById("lastName")
+const listItems = document.getElementById("list-items")
 
 function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!!")
+    if(firstName.value && lastName.value === ""){
+        alert("the input field cannot be empty")
     }
     else{
-        let li = document.createElement("li")
-        li.innerHTML = inputBox.value
-        listContainer.appendChild(li)
-        let span = document.createElement("span")
-        span.innerHTML = "\u00d7"
-        li.appendChild(span)
+       let li = document.createElement('li')
+       li.innerHTML = firstName.value
+       li.innerHTML = lastName.value
+       listItems.appendChild(li)
     }
-
-    inputBox.value = ""
-    saveData()
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked")
-        saveData()
-    }
-    else if(e.target.tagName === 'SPAN'){
-        e.target.parentElement.remove()
-        saveData()
-    }
-}, false)
-
-function saveData(){
-    localStorage.setItem('data', listContainer.innerHTML)
+function getTasks(){
+    localStorage.getItem('firstName')
 }
-function showData(){
-    listContainer.innerHTML = localStorage.getItem("data")
-}
-showData()
