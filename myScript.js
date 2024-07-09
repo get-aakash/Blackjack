48e4362c82b86446247c6e998ef241787ee0c83b
Aakash Acharya <mail.aakash108@gmail.com>
2024-07-09 22:14:31 +1000

    count app

--- myScript.js
+++ myScript.js
@@ -1,19 +1,8 @@
-const firstName = document.getElementById("firstName")
-const lastName = document.getElementById("lastName")
-const listItems = document.getElementById("list-items")
+let count = document.getElementById("numberOfCount").innerText
+let countTag = document.getElementById("numberOfCount") 
 
-function addTask(){
-    if(firstName.value && lastName.value === ""){
-        alert("the input field cannot be empty")
-    }
-    else{
-       let li = document.createElement('li')
-       li.innerHTML = firstName.value
-       li.innerHTML = lastName.value
-       listItems.appendChild(li)
-    }
-}
-
-function getTasks(){
-    localStorage.getItem('firstName')
+function countPeople(){
+    count++
+    console.log(count)
+    countTag.innerHTML = count
 }
