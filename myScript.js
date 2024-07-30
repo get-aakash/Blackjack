b72c8c8e5fa43d5486bbe46b2f26f90f79e0ded8
Aakash Acharya <mail.aakash108@gmail.com>
2024-07-30 17:50:14 +1000

    finishing the game

--- myScript.js
+++ myScript.js
@@ -8,22 +8,35 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector('#cards-el')
 
-function getRandomCard(){
-    let randomNumber =  Math.floor(Math.random() *13) + 1
-    
-    console.log(randomNumber)
-    if(randomNumber === 1){
-        return 11
-    }else if(randomNumber===11 || randomNumber===12 ||  randomNumber===13){
-        return 10
-    }else{
-        return randomNumber
-    }
-     
+
+let player = {
+     name : "Aakash",
+     chips : 200
+
 }
 
 
-function startGame(){
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+
+    console.log(randomNumber)
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber === 11 || randomNumber === 12 || randomNumber === 13) {
+        return 10
+    } else {
+        return randomNumber
+    }
+
+}
+console.log(cards)
+
+function startGame() {
     isALive = true
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
@@ -31,36 +44,37 @@ function startGame(){
     sum = firstCard + secondCard
     renderGame()
 }
-function renderGame(){
+function renderGame() {
     cardsEl.textContent = "Cards: "
-    for(let i=0; i<cards.length; i++){
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
-    if(sum <= 20){
+    if (sum <= 20) {
         message = "Do you want to draw a new card?"
-    }else if(sum === 21){
+    } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
-    }else{
+    } else {
         message = "You are out of the game!"
-        isALive= false
+        isALive = false
     }
 
     messageEl.textContent = message
-   
+
     sumEl.textContent = "Sum: " + sum
 }
 
-function newCard(){
+function newCard() {
+    if (isALive && !hasBlackJack) {
+        let card = getRandomCard()
+        sum = sum + card
+        cards.push(card)
+        console.log(cards)
+        renderGame()
 
-    let card = getRandomCard()
-    sum = sum + card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    }
+
 }
-
-
 
 
 
